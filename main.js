8ad3970b6e372b11bc2a66ee4e4ef864542866b2
Maki Miyagawa <61111655+mkmy1123@users.noreply.github.com>
2020-09-27 20:18:01 +0900

    refoctoring js_modal

--- main.js
+++ main.js
@@ -1,40 +1,16 @@
-function popupModalOne() {
-  var popup = document.getElementById('js-popup-1');
-  if (!popup) return;
+const openModalTriggers = document.querySelectorAll(".show-popup")
+const closeModalTriggers = document.querySelectorAll(".close")
 
-  var blackBg = document.getElementById('js-black-bg-1');
-  var closeBtn = document.getElementById('js-close-btn-1');
-  var showBtn = document.getElementById('js-show-popup-1');
+openModalTriggers.forEach((trigger, i) => {
+  trigger.addEventListener("click", () => {
+    const popups = document.querySelectorAll(".popup")
+    popups[i].classList.toggle('is-show')
+  })
+})
 
-  closePopUp(blackBg);
-  closePopUp(closeBtn);
-  closePopUp(showBtn);
-  function closePopUp(elem) {
-    if (!elem) return;
-    elem.addEventListener('click', function () {
-      popup.classList.toggle('is-show');
-    });
-  }
-}
-
-function popupModalTwo() {
-  var popup = document.getElementById('js-popup-2');
-  if (!popup) return;
-
-  var blackBg = document.getElementById('js-black-bg-2');
-  var closeBtn = document.getElementById('js-close-btn-2');
-  var showBtn = document.getElementById('js-show-popup-2');
-
-  closePopUp(blackBg);
-  closePopUp(closeBtn);
-  closePopUp(showBtn);
-  function closePopUp(elem) {
-    if (!elem) return;
-    elem.addEventListener('click', function () {
-      popup.classList.toggle('is-show');
-    });
-  }
-}
-
-popupModalOne();
-popupModalTwo();
+closeModalTriggers.forEach(trigger => {
+  trigger.addEventListener("click", e => {
+    const targetPopUp = e.target.closest(".popup")
+    targetPopUp.classList.toggle('is-show')
+  })
+})
