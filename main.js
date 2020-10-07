a1bf75bcde6ed4d58dd4b4b560ef3c56839c8602
Maki Miyagawa <61111655+mkmy1123@users.noreply.github.com>
2020-10-07 20:40:13 +0900

    finish arranging reviewed by member

--- main.js
+++ main.js
@@ -14,3 +14,23 @@ closeModalTriggers.forEach(trigger => {
     targetPopUp.classList.toggle('is-show')
   })
 })
+
+$(function () {
+  $(".btn-gnavi").on("click", function () {
+    // ハンバーガーメニューの位置を設定
+    var rightVal = 0;
+    if ($(this).hasClass("open")) {
+      // 位置を移動させメニューを開いた状態にする
+      rightVal = -300;
+      // メニューを開いたら次回クリック時は閉じた状態になるよう設定
+      $(this).removeClass("open");
+    } else {
+      // メニューを開いたら次回クリック時は閉じた状態になるよう設定
+      $(this).addClass("open");
+    }
+
+    $("#global-navi").stop().animate({
+      right: rightVal
+    }, 200);
+  });
+});
